f5acf18c479a8a31232a5a8de891bea757d8df6e
Jared Jewitt <jared@freshworks.io>
2020-03-27 15:07:26 -0700

    Added header (#3)

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,25 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-const Home = props => (
-  <div>
-    <h1>Basic React App</h1>
-    {props.name && <p>Hello, {props.name}!</p>}
-    <Link to="/about-us">Go to About Us</Link>
+const Home = () => (
+  <div className="home">
+    <div className="home__header">
+      <div className="home__header__container container">
+        <div className="home__header__container__logo">
+          <a
+            className="home__header__container__logo__title"
+            title="bc211"
+            href="http://www.bc211.ca"
+          >
+            bc211
+          </a>
+        </div>
+      </div>
+    </div>
+
+    <div className="home__content">
+      TODO
+    </div>
   </div>
 );
 
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 
 import Routes from "./Routes";
+import "./styles/index.scss";
 
 const App = () => (
   <BrowserRouter>
