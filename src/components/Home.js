c2e865824bb5a1c7c31e8cdcaa4fd28d941162a7
Jared Jewitt <jared@freshworks.io>
2020-03-27 15:11:01 -0700

    Update content

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,19 +6,35 @@ const Home = () => (
     <div className="home__header">
       <div className="home__header__container container">
         <div className="home__header__container__logo">
-          <a
+          <Link
             className="home__header__container__logo__title"
             title="bc211"
-            href="http://www.bc211.ca"
+            to="http://www.bc211.ca"
           >
             bc211
-          </a>
+          </Link>
         </div>
       </div>
     </div>
 
-    <div className="home__content">
-      TODO
+    <div className="home__content container">
+      <h1 className="home__content__title">Safe Seniors, Strong Communities</h1>
+      <p className="home__content__paragraph1">
+        A program that matches seniors who need support with non-medical
+        essentials, to volunteers in their community that are willing to help.
+      </p>
+      <p className="home__content__paragraph2">
+        To register for services, or to offer help, please fill in this&nbsp;
+        <Link
+          className="home__content__paragraph2__link"
+          to="https://forms.gle/V3yADFxST9Urp2pD7"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          form
+        </Link>
+        .
+      </p>
     </div>
   </div>
 );
