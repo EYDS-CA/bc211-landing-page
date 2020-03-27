08ce15d8bf799a365956830380b27663c4b62b07
Jared Jewitt <jared@freshworks.io>
2020-03-27 14:50:56 -0700

    Added header

--- webpack.config.js
+++ webpack.config.js
@@ -1,105 +1,88 @@
-const path = require("path");
-const webpack = require("webpack");
-const dotenv = require("dotenv");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-const HOST = process.env.HOST || "0.0.0.0";
+const HOST = process.env.HOST || '0.0.0.0';
 const PORT = process.env.PORT || 3000;
 
-const env = dotenv.config().parsed || {};
-
-const API_URL = env.API_URL || "http://localhost:5000/api";
-const GOOGLE_SIGNIN_CLIENT = env.GOOGLE_SIGNIN_CLIENT || "";
-
-const ENVIRONMENT_VARIABLES = {
-  "process.env.API_URL": JSON.stringify(API_URL),
-  "process.env.GOOGLE_SIGNIN_CLIENT": JSON.stringify(GOOGLE_SIGNIN_CLIENT),
-};
-
-module.exports = [
+// Loaders
+const getLoaders = () => [
   {
-    name: "dev",
-    entry: "./src/index.js",
-    mode: "development",
-    module: {
-      rules: [
-        {
-          test: /\.(js|jsx)$/,
-          exclude: /node_modules/,
-          use: ["babel-loader"],
-        },
-        {
-          test: /\.s[ac]ss$/i,
-          use: ["style-loader", "css-loader", "sass-loader"],
-        },
-      ],
-    },
-    resolve: {
-      extensions: ["*", ".js", ".jsx"],
-      alias: {
-        "@": path.join(__dirname, "src"),
-      },
-    },
-    output: {
-      path: path.join(__dirname, "dist"),
-      publicPath: "./",
-      filename: "bundle.js",
-    },
-    plugins: [
-      new CleanWebpackPlugin(),
-      new HtmlWebpackPlugin({
-        title: "Basic React App",
-        template: "./src/index.html",
-      }),
-      new webpack.DefinePlugin(ENVIRONMENT_VARIABLES),
-      new webpack.HotModuleReplacementPlugin(),
-    ],
-    devServer: {
-      contentBase: "./dist",
-      hot: true,
-      historyApiFallback: true,
-      writeToDisk: true,
-      host: HOST,
-      port: PORT,
-    },
+    test: /\.(js|jsx)$/,
+    exclude: /node_modules/,
+    use: ['babel-loader'],
   },
   {
-    name: "prod",
-    entry: "./src/index.js",
-    mode: "production",
-    module: {
-      rules: [
-        {
-          test: /\.(js|jsx)$/,
-          exclude: /node_modules/,
-          use: ["babel-loader"],
-        },
-        {
-          test: /\.s[ac]ss$/i,
-          use: ["style-loader", "css-loader", "sass-loader"],
-        },
-      ],
-    },
-    resolve: {
-      extensions: ["*", ".js", ".jsx"],
-      alias: {
-        "@": path.join(__dirname, "src"),
-      },
-    },
-    output: {
-      path: path.join(__dirname, "dist"),
-      publicPath: "./",
-      filename: "bundle.js",
-    },
-    plugins: [
-      new CleanWebpackPlugin(),
-      new HtmlWebpackPlugin({
-        // TODO: Add cache-buster (if needed)
-        title: "Basic React App",
-        template: "./src/index.html",
-      }),
-      new webpack.DefinePlugin(ENVIRONMENT_VARIABLES),
-    ],
+    test: /\.s[ac]ss$/i,
+    use: ['style-loader', 'css-loader', 'sass-loader'],
+  },
+  {
+    test: /\.css$/i,
+    use: [MiniCssExtractPlugin.loader],
+  },
+  {
+    test: /\.(jpe?g|png|gif|woff|woff2|eot|ttf|svg)(\?[a-z0-9=.]+)?$/,
+    loader: 'url-loader?limit=100000',
   },
 ];
+
+// Plugins
+const getPlugins = (isDevelopment) => [
+  new CleanWebpackPlugin(),
+  new HtmlWebpackPlugin({
+    title: 'BC211 Landing Page',
+    template: path.join(__dirname, 'public', 'index.html'),
+  }),
+  new MiniCssExtractPlugin({
+    filename: isDevelopment ? '[name].css' : '[name].[hash].css',
+    chunkFilename: isDevelopment ? '[id].css' : '[id].[hash].css',
+  }),
+];
+
+// Inherited base configuration
+const config = {
+  entry: './src/index.js',
+  resolve: {
+    extensions: ['*', '.js', '.jsx'],
+    alias: {
+      '@': path.join(__dirname, 'src'),
+    },
+  },
+  output: {
+    path: path.join(__dirname, 'dist'),
+    publicPath: '/',
+    filename: 'bundle.js',
+  },
+};
+
+const prodConfig = {
+  ...config,
+  name: 'prod',
+  mode: 'production',
+  module: {
+    rules: getLoaders(),
+  },
+  plugins: getPlugins(false),
+  performance: { hints: false },
+};
+
+const devConfig = {
+  ...config,
+  name: 'dev',
+  mode: 'development',
+  module: {
+    rules: getLoaders(),
+  },
+  plugins: getPlugins(true),
+  devServer: {
+    contentBase: './dist',
+    hot: true,
+    historyApiFallback: true,
+    writeToDisk: true,
+    host: HOST,
+    port: PORT,
+  },
+};
+
+module.exports = [prodConfig, devConfig];
