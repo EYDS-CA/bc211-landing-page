7ce91e5c95c0c616fbe8580531b8bf76de28fc60
Hitankar Ray <hitankar@freshworks.io>
2020-04-24 15:54:02 -0700

    conflict fix

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -150,11 +150,7 @@ const Home = () => {
 
           <Grid item xs={12} md={6} >
             <Card className={classes.card} variant="outlined">
-<<<<<<< Updated upstream
-              <CardContent display="flex" >
-=======
               <CardContent >
->>>>>>> Stashed changes
                 <img width="40" height="40" src={IconAlert} />
                 <Typography className={classes.cardTitle}>
                   Coronavirus Outbreak Information
